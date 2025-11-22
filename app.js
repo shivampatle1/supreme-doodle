f855121a4087f491c44ad69d08ba27982822aa7b
shivam <shivampatle12345@gmail.com>
2025-11-22 13:40:50 +0530

    second  commit

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ let firstName = "John";
 let lastName = "Doe";
 
 console.log(`Hello, ${firstName} ${lastName}!`);
+
+console.log(`Welcome to the application, ${firstName}.`);
+
+function getFullName() {
+    return `${firstName} ${lastName}`;
+}
+
+console.log(`User's full name is: ${getFullName()}`);
